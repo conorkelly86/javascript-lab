29a7d7e4bfe4273032884bcd028006ea91aeb6ed
conye9980 <conorkelly86@gmail.com>
2019-11-25 17:21:09 +0000

    Template Literals testing

---
+++ Fundamentals/Template Literals/TemplateLiterals.js
@@ -0,0 +1,11 @@
+let a = "This";
+let b = "is";
+let c = "multiple";
+let d = "variables";
+let e = "testing";
+let f = "template";
+let g = "literals";
+
+let x = `${a} ${b} ${c} ${d} ${e} ${f} ${g}`;
+
+console.log(x);
