7491ce76ea1aa90b3746784b7350fcc2d6f021e5
conye9980 <conorkelly86@gmail.com>
2019-11-26 12:35:49 +0000

    onclick Events practice

---
+++ Fundamentals/onclick Events/js.js
@@ -0,0 +1,12 @@
+function myFunction1() {
+  document.getElementById("para").innerHTML = "Hello Peter";
+}
+function myFunction2() {
+  document.getElementById("para").innerHTML = "Hello T'Challa";
+}
+function myFunction3() {
+  document.getElementById("para").innerHTML = "Hello Steve";
+}
+function myFunction4() {
+  document.getElementById("para").innerHTML = "Hello Tony";
+}
